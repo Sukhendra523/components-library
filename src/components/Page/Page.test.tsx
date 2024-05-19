26e4cbf6214b1f92aa55f0dd48e166581a203866
sukhendra523 <523sukh@gmail.com>
2024-05-19 22:11:19 +0530

    Added Jest unit UI Testing Cases

---
+++ src/components/Page/Page.test.tsx
@@ -0,0 +1,27 @@
+import "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
+import Page from ".";
+
+describe("Page", () => {
+  it("render title and children", () => {
+    const title = "this title";
+    const children = "this is children";
+    const { getByText } = render(<Page title={title}>{children}</Page>);
+
+    const titleElm = getByText(title);
+    const childrenElm = getByText(children);
+
+    expect(titleElm).toBeInTheDocument();
+    expect(childrenElm).toBeInTheDocument();
+  });
+
+  it("render correct styling", () => {
+    const title = "this title";
+    const children = "this is children";
+    const { getByTestId } = render(<Page title={title}>{children}</Page>);
+
+    const container = getByTestId('page-container')
+
+    expect(container).toHaveStyle('background-color: #f5f5f5')
+  });
+});

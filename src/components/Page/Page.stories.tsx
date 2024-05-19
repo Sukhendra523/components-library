c2f3503b85adc6d5bf90b2309f92561c7ded3079
sukhendra523 <523sukh@gmail.com>
2024-05-19 22:55:24 +0530

    Feat: Added StoryBook

---
+++ src/components/Page/Page.stories.tsx
@@ -0,0 +1,42 @@
+import Page from ".";
+
+export default {
+    title:"Components/Page",
+    component: Page,
+    parameters: {
+        layout: "centered"
+      },
+      tags: ["autodocs"],
+
+    argTypes: {
+        title :{
+            description:"This is Title of Page",
+            control:{
+                type:'text'
+            }
+        },
+        children :{
+            description:"This is children of Page",
+            control:{
+                type:'text'
+            }
+        }
+    },
+
+}
+
+
+
+export const PageOne = {
+    args: {
+      title: "This is the Page One title",
+      children: "These are the Page One children"
+    }
+  };
+  
+  export const PageTwo = {
+    args: {
+      title: "This is the Page Twoooooooooo title",
+      children: "These are the Page Two children"
+    }
+  };
